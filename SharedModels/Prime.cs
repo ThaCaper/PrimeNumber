2abefa7366b6618613e7977dea64ba8b9def5a17
MikkelFLK <33052374+MikkelFLK@users.noreply.github.com>
2021-03-24 11:17:51 +0100

    SharedModels

---
+++ SharedModels/Prime.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedModels
+{
+    public class Prime
+    {
+        int Input;
+        bool isPrime;
+        int Occurrences;
+    }
+}
---
+++ SharedModels/PrimeRange.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedModels
+{
+    public class PrimeRange
+    {
+        int Input1;
+        int Input2;
+        int NumOfPrimes;
+        int Occurrences;
+    }
+}
